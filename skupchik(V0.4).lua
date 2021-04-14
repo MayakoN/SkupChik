99d4adc09a9e47cf9bc03fd3b20767b891b15a35
MayakoN <78068546+MayakoN@users.noreply.github.com>
2021-04-14 23:25:25 +0500

    Delete skupchik(V0.4).lua

--- skupchik(V0.4).lua
+++
@@ -1,9 +0,0 @@
-function main()
-    while not isSampAvailable() do wait(0) end
-    msg("Скрипт загружен.")
-    msg("Автор скрипта: {FF06FB}Mayako")
-    while true do
-        wait(0)
-        
-    end
-end
