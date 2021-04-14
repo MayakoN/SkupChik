8fdaca1da33a0299b4a2e956925da97dd51d3d8e
MayakoN <78068546+MayakoN@users.noreply.github.com>
2021-04-14 13:48:09 +0500

    Update skupchik(V0.4).lua

--- skupchik(V0.4).lua
+++ skupchik(V0.4).lua
@@ -1,1 +1,9 @@
-
+function main()
+    while not isSampAvailable() do wait(0) end
+    msg("Скрипт загружен.")
+    msg("Автор скрипта: {FF06FB}Mayako")
+    while true do
+        wait(0)
+        
+    end
+end
